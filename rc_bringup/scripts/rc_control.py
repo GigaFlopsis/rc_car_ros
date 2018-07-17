cca1a4905338183b18ed81b0998141ca2dd19efc
Devitt Dmitry <giga.flopsis@gmail.com>
2018-07-17 08:51:28 +0300

    Update rc_control.py

--- rc_bringup/scripts/rc_control.py
+++ rc_bringup/scripts/rc_control.py
@@ -481,7 +481,6 @@ if __name__ == "__main__":
                 time_clb += 0.2
 
                 if time_clb < 1.0 and motor_run:
-                    pass
 		    set_rc_remote(current_mode)     # set pwm mode
 
                 else:           # not cld remote data break pwm
