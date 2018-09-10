09a58c83d3ba95e68490cd0112a32cbe40346066
GearMaker <ddimonxt@gmail.com>
2018-09-10 16:16:44 +0300

    Fixed matrix for potential fields

--- rc_bringup/scripts/pose_controller_with_reg_func4.py
+++ rc_bringup/scripts/pose_controller_with_reg_func4.py
@@ -130,73 +130,84 @@ def get_distance_to(a,b):
     dist = np.linalg.norm(pos)
     return dist
 
-#def plan_virtual_fields():
-#    global goal_pose, goal_topic, first_waypoint_in_route, Frep, nearest_point, dist_goal, Obs_xy, dt, i, nearest_point
-#    coordinates_obstacles()
-#    Frep = [0,0]
-#    nearest_point = None
-#    dist_goal = get_distance_to(current_pose, goal_pose)
-#    k=2
-#    Fatt=k*(get_distance_to(goal_pose,current_pose))/dist_goal
- #   print("Fatt", Fatt)
- #   if len(Obs_xy)==0:
- #       min_d = math.inf
- #       for i in range(len(Obs_xy)):
- #           d = math.sqrt((current_pose.position.x - Obs_xy[i][1]) ** 2 + (current_pose.position.y - Obs_xy[i][2]) ** 2)
- #           if d < 1.5:
- #               if min_d > d:
- #                   min_d = d
- #                   nearest_point = [Obs_xy[i][1],Obs_xy[i][2]]
- #       c=2
- #       if nearest_point == None:
- #           Frep=c*(current_pose-nearest_point)/min_d**2
- #   r=10
- #   x_new=current_pose.position.x+(Fatt[0]+Frep[0])*dt*r
- #   print("new_current_x",x_new)
- #   y_new=current_pose.position.y+(Fatt[1]+Frep[1])*dt*r
- #   print("new_current_x",y_new)
- #   print("x_new", x_new)
- #   print("y_new", y_new)
- #   goal_pose.position.x=x_new
- #   goal_pose.position.y=y_new
- #   print("goal_pose",goal_pose)
- #   return goal_pose
+def plan_virtual_fields():
+    global goal_pose, goal_topic, first_waypoint_in_route, Frep, nearest_point, dist_goal, Obs_xy, dt, i, nearest_point
+    coordinates_obstacles()
+    nearest_point = None
+    Frep=[0,0]
+    dist_goal = get_distance_to(current_pose, goal_pose)
+    k=2
+    goal_p=np.array([goal_pose.position.x,goal_pose.position.y])
+    current_p=np.array([current_pose.position.x,current_pose.position.y])
+   # print("goal_p =",goal_p)
+    Fatt=k*(goal_p-current_p)/dist_goal
+   # print("Fatt", Fatt)
+    if len(Obs_xy)!=0:
+        min_d = float('inf')
+        for i in range(len(Obs_xy)):
+            d = math.sqrt((current_pose.position.x - Obs_xy[0][i]) ** 2 + (current_pose.position.y - Obs_xy[1][i]) ** 2)
+            if d < 1.5:
+                if min_d > d:
+                    min_d = d
+                    nearest_point = [Obs_xy[0][i],Obs_xy[1][i]]
+        c=2
+        if nearest_point != None:
+            
+            print("min_d",min_d)
+            current_po=[current_pose.position.x,current_pose.position.y]
+            print("current_po= ",current_po)
+            print("nearest_point= ", nearest_point)
+            Frep_x=c*(current_po[0]-nearest_point[0])/min_d**2
+            Frep_y=c*(current_po[1]-nearest_point[1])/min_d**2
+            Frep=[Frep_x,Frep_y]
+            print("Frep =",Frep)
+            
+    r=10
+   # print("Frep_x =",Frep[0])
+   # print("Frep_y =",Frep[1])
+   # print("Fatt =",Fatt)
+   # print("current_x =", current_pose.position.x)
+    x_new=current_pose.position.x+(Fatt[0]+Frep[0])*dt*r
+   # print("new_current_x",x_new)
+    y_new=current_pose.position.y+(Fatt[1]+Frep[1])*dt*r
+   # print("new_current_x",y_new)
+   # print("x_new", x_new)
+   # print("y_new", y_new)
+    goal_pose.position.x=x_new
+    goal_pose.position.y=y_new
+    print("goal_pose",goal_pose)
+    return goal_pose
 
-#def coordinates_obstacles():
-#    global current_course, Obs_xy, lid_and_vec, lidar_arr, xn, yn, x_matrix, y_matrix, phi_new_vec, lid_ang_vec, phi_new_x, phi_new_y, lid_new_x, lid_new_y
-#    alpha=math.radians(360)
-#    step=math.radians(1)
-#    lid = np.arange(-alpha/2,alpha/2,step)
-#    lid_ang_vec =np.transpose(lid)
-#    phi_vec_1 = np.ones((alpha/step,1))*current_course
-#    phi_vec = np.transpose(phi_vec_1)
-#    phi_vec = phi_vec[0]
-#    x_matrix=np.ones((360,1))*current_pose.position.x
-#    y_matrix=np.ones((360,1))*current_pose.position.y
-#    print(x_matrix)
-#    print(y_matrix)
-#    for j in range(len(phi_vec)):
-#        phi_new_y.append(math.sin(phi_vec[j]-lid_ang_vec[j]))
-#        phi_new_x.append(math.cos(phi_vec[j]-lid_ang_vec[j]))
-#        lid_new_x.append(lidar_arr[j]*phi_new_x[j])
-#        lid_new_y.append(lidar_arr[j]*phi_new_y[j])
-#    print("phi_new_x", phi_new_x)
-#    print("phi_new_y", phi_new_y)
-#    print("lid_new_x", lid_new_y)
-#    print("lid_new_y", lid_new_y)
-   # for k in range(len(phi_vec)):
-   #     lid_new_x.append(lidar_arr[k]*phi_new_x[k])
-   #     lid_new_y.append(lidar_arr[k]*phi_new_y[k])
-#    yn = lid_new_y + y_matrix[0]
-#    xn = lid_new_x + x_matrix[0]
-   # print("yn",yn)
-   # print("xn",xn)
-   # print("leinght y",len(lid_new_y))
-   # print("leinght x",len(lid_new_x))
-   # print("x_mat",len(x_matrix))
-   # print("y_mat",len(y_matrix))
-#    Obs_xy = [xn,yn]
-#    print("Obs_xy",Obs_xy)
+def coordinates_obstacles():
+    global current_course, Obs_xy, lid_and_vec, lidar_arr, xn, yn, x_matrix, y_matrix, phi_new_vec, lid_ang_vec, phi_new_x, phi_new_y, lid_new_x, lid_new_y
+    alpha=math.radians(360)
+    step=math.radians(1)
+    lid = np.arange(-alpha/2,alpha/2,step)
+    lid_ang_vec =np.transpose(lid)
+    phi_vec_1 = np.ones((alpha/step,1))*current_course
+    phi_vec = np.transpose(phi_vec_1)
+    phi_vec = phi_vec[0]
+    x_matrix=np.ones((360,1))*current_pose.position.x
+    y_matrix=np.ones((360,1))*current_pose.position.y
+    #print(x_matrix)
+    #print(y_matrix)
+    for j in range(len(phi_vec)):
+        phi_new_y.append(math.sin(phi_vec[j]-lid_ang_vec[j]))
+        phi_new_x.append(math.cos(phi_vec[j]-lid_ang_vec[j]))
+        lid_new_x.append(lidar_arr[j]*phi_new_x[j])
+        lid_new_y.append(lidar_arr[j]*phi_new_y[j])
+    #print("phi_new_x", phi_new_x)
+    #print("phi_new_y", phi_new_y)
+    #print("lid_new_x", lid_new_y)
+    #print("lid_new_y", lid_new_y)
+    for k in range(len(phi_vec)):
+        lid_new_x.append(lidar_arr[k]*phi_new_x[k])
+        lid_new_y.append(lidar_arr[k]*phi_new_y[k])
+    yn = lid_new_y + y_matrix[0]
+    xn = lid_new_x + x_matrix[0]
+
+    Obs_xy = [xn,yn]
+   # print("Obs_xy" ,Obs_xy[0][])
 
 def main():
     global dt, current_pose, current_course, goal_pose, cmd_vel_msg , u_v, u_rot, Ev, Erot,sumErot,sumEv, plot_x,plot_y, v_des, leinght_v,leinght_rot,v , finish_flag, goal_tolerance, dist, upper_limit_of_ki_sum, lower_limit_of_ki_sum, Ev_old, Erot_old
@@ -426,7 +437,7 @@ if __name__ == "__main__":
 
             old_ros_time = rospy.get_time()
             cmd_vel_msg = main()
-           # goal_pose_msg = plan_virtual_fields() 
+            goal_pose_msg = plan_virtual_fields() 
             if finish_flag:
                 if currentTime > 1.0:
                     print("pose controller: finish_flag True")
