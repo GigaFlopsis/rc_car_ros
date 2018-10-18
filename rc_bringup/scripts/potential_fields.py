71b41cc7ce52ba7d309691c8e1e050b97007b20d
Vladimir Pereverzev <pereverzevva@gmail.com>
2018-10-18 17:23:26 +0300

    Fixed msg format for potential_fields

--- rc_bringup/scripts/potential_fields.py
+++ rc_bringup/scripts/potential_fields.py
@@ -11,7 +11,7 @@ import tf
 import tf2_ros
 import sensor_msgs.point_cloud2 as pc2
 import laser_geometry.laser_geometry as lg
-from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+#from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 from geometry_msgs.msg import Twist, Pose, PoseStamped
 from sensor_msgs.msg import PointCloud2, LaserScan
 from rc_bringup.cfg import PoseControllerConfig
@@ -21,12 +21,12 @@ from pid_params_saver import YamlParams
 #value
 velocity = float()
 cmd_vel_msg = Twist()
-current_pose = Pose()
+current_pose = PoseStamped()
 current_course = float()
 
-goal_pose = Pose()
-goal_pose_msg = Pose()
-goal_new=Pose()
+goal_pose = PoseStamped()
+goal_pose_msg = PoseStamped()
+goal_new=PoseStamped()
 
 init_flag = False
 max_vel = 1.1 # m/s
@@ -39,10 +39,10 @@ dist=0.0
 
 #topics
 cmd_vel_topic = "/cmd_vel"
-vel_topic = "/mavros/local_position/velocity"
+vel_topic = "/mavros/goal/velocity"
 goal_topic = "/goal"
 local_goal_topic = "/local_goal"
-pose_topic = "/mavros/local_position/pose"
+pose_topic = "/pose"
 lidar_topic = "/scan"
 point_cloud2_topic="/laserPointCLoud"
 
@@ -106,7 +106,7 @@ def get_distance_to(a,b):
     :param dist: distance
     :return: dist
     """
-    pos = np.array([[b.position.x - a.position.x],
+    pos = np.array([[b.pose.position.x - a.position.x],
                     [b.position.y - a.position.y]])
     dist = np.linalg.norm(pos)
     return dist
@@ -144,7 +144,7 @@ def coordinates_obstacles():
     print('Phi_new_vec_y',len(phi_new_y))
 
     yn = lid_new_y
-    xn = lid_new_x 
+    xn = lid_new_x
     for i in range(len(xn)):
         if not np.isinf(xn[i]) and not np.isinf(yn[i]):
             xn_new.append(-xn[i])
@@ -159,8 +159,8 @@ def coordinates_obstacles():
     nearest_point=np.array([[xn_new[min_i]],
                        [yn_new[min_i]]])
     nearest_p_in_global=np.dot(rotate,nearest_point)
-    x=nearest_p_in_global[0]+current_pose.position.x
-    y=nearest_p_in_global[1]+current_pose.position.y
+    x=nearest_p_in_global[0]+current_pose.pose.position.x
+    y=nearest_p_in_global[1]+current_pose.pose.position.y
     Obs_xy=[x,y]
     print(Obs_xy)
 
@@ -168,13 +168,13 @@ def coordinates_obstacles():
 
 def plan_virtual_fields():
     global goal_pose, goal_topic,current_pose, first_waypoint_in_route, Frep, nearest_point, dist_goal, Obs_xy, dt, i, nearest_point, dist ,xn_new, yn_new, step_1, goal_new_p, Ev, Ev_old, sumEv, Erot, Erot_old, sumErot, finish_flag, range_dia, aproximation_point, lidar_arr_new, distance, point_cloud, Obs_xy
-    
+
     coordinates_obstacles()
-    if get_distance_to(current_pose,goal_pose)>0.2 and step_1==1:
+    if (np.sqrt((goal_pose.pose.position.x-current_pose.pose.position.x)**2+(goal_pose.pose.position.y-current_pose.pose.position.y)**2))>0.2 and step_1==1:
         if len(goal_new_p)<2:
-            goal_new_p.append(goal_pose.position.x)
-            goal_new_p.append(goal_pose.position.y)
-    dist=np.sqrt((goal_new_p[0]-current_pose.position.x)**2+(goal_new_p[1]-current_pose.position.y)**2)
+            goal_new_p.append(goal_pose.pose.position.x)
+            goal_new_p.append(goal_pose.pose.position.y)
+    dist=np.sqrt((goal_new_p[0]-current_pose.pose.position.x)**2+(goal_new_p[1]-current_pose.pose.position.y)**2)
     print("dist= ",dist)
 
     if (abs(dist) < goal_tolerance):
@@ -183,30 +183,30 @@ def plan_virtual_fields():
     print("step_1",step_1)
     print("goal_new_p",goal_new_p)
     Frep=[0,0]
-    dist_goal = get_distance_to(current_pose, goal_pose)
+    dist_goal = np.sqrt((-goal_pose.pose.position.x+current_pose.pose.position.x)**2+(-goal_pose.pose.position.y+current_pose.pose.position.y)**2)
     k=2
 
-    goal_p=np.array([goal_pose.position.x,goal_pose.position.y])
-    current_p=np.array([current_pose.position.x,current_pose.position.y])
+    goal_p=np.array([goal_pose.pose.position.x,goal_pose.pose.position.y])
+    current_p=np.array([current_pose.pose.position.x,current_pose.pose.position.y])
 
     Fatt=k*(goal_new_p-current_p)/dist_goal #goal_p
     print("goal_p",goal_p)
-    print("current_p",current_p)
+    print("current_p",current_pose)
     print("Fatt", len(Fatt))
     print('xn_new_len',len(xn_new))
     if len(xn_new)!=0:
         nearest_point=None
         min_d = np.inf
-        d = math.sqrt((current_pose.position.x - Obs_xy[0]) ** 2 + (current_pose.position.y - Obs_xy[1]) ** 2)
+        d = math.sqrt((current_pose.pose.position.x - Obs_xy[0]) ** 2 + (current_pose.pose.position.y - Obs_xy[1]) ** 2)
         if d < 1.5:
-            if (abs(current_course-(math.atan2(Obs_xy[1]-current_pose.position.y,Obs_xy[0]-current_pose.position.x))))<math.pi/2:
+            if (abs(current_course-(math.atan2(Obs_xy[1]-current_pose.pose.position.y,Obs_xy[0]-current_pose.pose.position.x))))<math.pi/2:
                 if min_d > d:
                     min_d = d
                     nearest_point = [Obs_xy[0],Obs_xy[1]]
-                   
+
                 c=3 #0.01 10
                 if nearest_point!=None:
-                    current_po=[current_pose.position.x,current_pose.position.y]
+                    current_po=[current_pose.pose.position.x,current_pose.pose.position.y]
                     Frep_x=c*(current_po[0]-nearest_point[0])/min_d**2
                     Frep_y=c*(current_po[1]-nearest_point[1])/min_d**2
                 Frep=[Frep_x,Frep_y]
@@ -217,11 +217,10 @@ def plan_virtual_fields():
     print("Frep =", Frep)
     print("Fatt =", Fatt)
 
-    x_new=current_pose.position.x+(Fatt[0]+Frep[0])*dt*r
-    y_new=current_pose.position.y+(Fatt[1]+Frep[1])*dt*r
-
-    goal_pose.position.x=x_new
-    goal_pose.position.y=y_new
+    x_new=current_pose.pose.position.x+(Fatt[0]+Frep[0])*dt*r
+    y_new=current_pose.pose.position.y+(Fatt[1]+Frep[1])*dt*r
+    goal_pose.pose.position.x=x_new
+    goal_pose.pose.position.y=y_new
     xn_new=list()
     yn_new=list()
     distance=list()
@@ -234,11 +233,12 @@ def plan_virtual_fields():
 #    print(point_cloud2)
 
 def goal_clb(data):
-    #Get goal pose
+    #Get goal poserr
     global goal_pose, init_flag, finish_flag
     if goal_pose!=0:
         rospy.sleep(0)
-    goal_pose=data.pose
+    print(type(goal_pose))
+    goal_pose=data
     init_flag = True
     finish_flag = False
 
@@ -246,7 +246,7 @@ def current_pose_clb(data):
     #Get current pose from topic
     global current_pose, current_course
 
-    current_pose = data.pose
+    current_pose = data
     rot = [data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w]
     # # convert euler from quaternion
     (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(rot)
@@ -264,11 +264,11 @@ if __name__ == "__main__":
     rospy.init_node('potential_fields', anonymous=True)
     rate = rospy.Rate(2)  # 10hz
 
-    #rospy.Subscriber(goal_topic, PoseStamped, goal_clb)
+    rospy.Subscriber(goal_topic, PoseStamped, goal_clb)
     rospy.Subscriber(pose_topic, PoseStamped, current_pose_clb)
     rospy.Subscriber(lidar_topic, LaserScan, laser_scan_clb)
 
-    new_goal_pub = rospy.Publisher(goal_topic, Pose, queue_size=10)
+    new_goal_pub = rospy.Publisher(goal_topic, PoseStamped, queue_size=10)
     new_vel_pub = rospy.Publisher(cmd_vel_topic,Twist,queue_size=10)
     listener = tf.TransformListener()
     old_ros_time = rospy.get_time()
@@ -283,29 +283,22 @@ if __name__ == "__main__":
 
             if(not init_flag):
                 if currentTime > 1.0:
-                    #print("pose controller: not init")
+                    print("pose controller: not init")
                     currentTime =  0.0
                 continue
 
             old_ros_time = rospy.get_time()
 
             goal_pose_msg = plan_virtual_fields()
-            step_1=step_1+1 
+            step_1=step_1+1
             if finish_flag:
                 if currentTime > 1.0:
-                    #print("pose controller: finish_flag True")
+                    print("pose controller: finish_flag True")
                     currentTime = 0.0
-                    cmd_vel_msg.linear.x = 0.0
-                init_flag = False
-                new_vel_pub.publish(cmd_vel_msg)
 
-            #new_goal_pub.publish(goal_pose_msg) # publish msgs to the robot
+                init_flag = False
+            new_goal_pub.publish(goal_pose_msg)
             rate.sleep()
 
     except KeyboardInterrupt:   # if put ctr+c
         exit(0)
-
-
-
-
-
